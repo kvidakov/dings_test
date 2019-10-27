a3a78abe93835467a3781f5d256eb45588b6e6fc
kvidakov <vidakovic.karlo@gmail.com>
2019-10-27 17:03:19 +0100

    fix(Prizes): remove <b-card-group> and fix mobile layout

--- src/components/Prizes.vue
+++ src/components/Prizes.vue
@@ -2,23 +2,21 @@
   <b-container class="prizes" fluid>
     <h1 class="my-5">Nagrade</h1>
     <b-row>
-      <b-card-group class="row" deck>
-        <template v-for="prize in prizes">
-          <b-col :key="prize.id" cols="3">
-            <b-card
-              :title="prize.name"
-              :img-src="generatePrizeImgUrl(prize.image_uri)"
-              :img-alt="prize.name"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="col-3 mb-2"
-            >
-              <b-button @click="goToPrizeDetails(prize.id, prize.name, generatePrizeImgUrl(prize.image_uri))" variant="primary">Detalji</b-button>
-            </b-card>
-          </b-col>
-        </template>
-      </b-card-group>
+      <template v-for="prize in prizes">
+        <b-col :key="prize.id" cols="12" md="3">
+          <b-card
+            :title="prize.name"
+            :img-src="generatePrizeImgUrl(prize.image_uri)"
+            :img-alt="prize.name"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-3"
+          >
+            <b-button @click="goToPrizeDetails(prize.id, prize.name, generatePrizeImgUrl(prize.image_uri))" variant="primary">Detalji</b-button>
+          </b-card>
+        </b-col>
+      </template>
     </b-row>
   </b-container>
 </template>
