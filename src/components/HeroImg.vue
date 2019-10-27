9bcab27fe6bad833e3041492523de66324628fda
kvidakov <vidakovic.karlo@gmail.com>
2019-10-27 17:02:46 +0100

    fix(HeroImg): add styles for mobile

--- src/components/HeroImg.vue
+++ src/components/HeroImg.vue
@@ -19,20 +19,32 @@ export default {
 
 <style scoped>
   .jumbotron {
-    background: url('../assets/beer_and_rosemary.jpeg') center center / cover no-repeat;
+    background: url('../assets/beer_and_rosemary.jpeg') 58% center / cover no-repeat;
     color: #fff;
     height: 100vh;
-    padding-top: 10%;
     text-align: left;
   }
 
   h1 {
     font-weight: 600;
+    margin-top: 5%;
     text-transform: uppercase;
   }
 
   p {
     font-size: 60px;
     font-weight: 600;
+  }
+
+  @media only screen and (max-width: 768px) {
+    h1.display-3 {
+      font-size: 50px;
+      margin-top: 10%;
+    }
+
+    p {
+      font-size: 35px;
+      font-weight: 600;
+    }
   }
 </style>
